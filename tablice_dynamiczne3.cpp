c71d2d6167f7c052701106fb0ede526a9d073ace
Bartosz Wiaderny <10376794u@qed.local>
2024-09-25 11:23:22 +0200

    dodanie

---
+++ tablice_dynamiczne3.cpp
@@ -0,0 +1,59 @@
+
+
+#include <iostream>
+using namespace std;
+class kalsa{
+    public:
+    void szukanie(){
+        int w;
+        char p;
+        cout<<"podaj wielkosc:";
+        cin>>w;
+      
+        char *tab=new char[w];
+        char *tab2=new char[w];
+    
+        
+        for(int i = 0;i<w;i++){
+            cout<<"podaj char dla tablicy tab:";
+            cin>>tab[i];
+        }
+        
+        for(int i = 0;i<w;i++){
+            cout<<"podaj char dla tablicy tab2:";
+            cin>>tab2[i];
+        }
+        
+       
+        
+        cout<<"\ntab:";
+        for(int i = 0;i<w;i++){
+            cout<<tab[i]<<" ";
+        }
+        
+        cout<<"\ntab2:";
+        for(int i = 0;i<w;i++){
+            cout<<tab2[i]<<" ";
+        }
+        
+        cout<<"\npowtarzajace sie elementy:";
+        for(int i = 0 ;i<w;i++){
+            if(tab[i]=tab2[i]){
+                
+                cout<<tab[i]<<" ";
+            }
+        }
+     
+        
+        delete[] tab;
+        delete[] tab2;
+    
+    }
+};
+
+int main(){
+    kalsa k;
+    k.szukanie();
+
+    return 0;
+}
